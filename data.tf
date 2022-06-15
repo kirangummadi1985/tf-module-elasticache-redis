62bbe8580f88bd09086e863507f4d199dd5e0fda
kiran <kirangummadi1985@gmail.com>
2022-06-15 06:30:14 +0530

    elasticache-redis

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-devopsb64"
-    key    = "vpc/dev/${var.COMPONENT}/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#data "terraform_remote_state" "vpc" {
+#  backend = "s3"
+#  config = {
+#    bucket = "terraform-devopsb64"
+#    key    = "vpc/dev/${var.COMPONENT}/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
